2f769a90ad07ad2ec8bd6f254f721edb02bcc7fd
daliaalaa <daliaelhefnii@gmail.com>
2020-04-29 22:45:30 +0200

    Adding all-orders component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AllOrdersComponent } from './components/all-orders/all-orders.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllOrdersComponent
   ],
   imports: [
     BrowserModule,
